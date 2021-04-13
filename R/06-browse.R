6180dfe1aa137bd65b49e9ceff8d97f1c352b73b
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2021-04-13 12:58:40 +0100

    cran comments on the documentation of @return

--- R/06-browse.R
+++ R/06-browse.R
@@ -1,9 +1,9 @@
 #' Quickly browse to ONS' developer webpage
 #'
-#' This function take you to the ONS' developer webpage and
-#' return the URL invisibly.
+#' This function take you to the ONS' developer webpage.
 #'
 #' @export
+#' @return An atomic character vector with the url of the webpage
 #' @examples
 #' \donttest{
 #' ons_browse()
@@ -14,15 +14,17 @@ ons_browse <- function() {
 
 #' Quickly browse to dataset's Quality and Methodology Information (QMI)
 #'
-#' This function take you to the QMI and return the URL invisibly.
+#' This function take you to the QMI.
 #'
 #' @inheritParams ons_get
 #' @export
+#' @return An atomic character vector url with of the webpage
 #' @examples
 #' \donttest{
 #' ons_browse_qmi("cpih01")
 #' }
 ons_browse_qmi <- function(id = NULL) {
+  assert_valid_id(id)
   ons_ds <- ons_datasets()
   id_num <- id_number(id, ons_ds)
   view_url(ons_ds$qmi$href[id_num])
